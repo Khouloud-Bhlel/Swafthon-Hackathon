e9f9633fe19b448b787a8f75e3e15119da673d1a
khouloud ben hlel <khouloudbenhelal24@gmail.com>
2024-11-09 21:20:31 +0100

    page home,about,events,clubs,login,register,chatbot

--- swaf/hackathon/urls.py
+++ swaf/hackathon/urls.py
@@ -5,4 +5,11 @@ app_name = 'hackathon'
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('register/', views.register_page, name='register'),
+    path('event/', views.event_page, name='event'),
+    path('login/', views.login_page, name='login'),
+    path('club/', views.club_page, name='club'),
+    path('events/', views.events_page, name='events'),
+    path('about/', views.about_page, name='about'),
+    path('chatbot/', views.chatbot_page, name='chatbot'),
 ]
--- swaf/hackathon/views.py
+++ swaf/hackathon/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'HomePage.html')
+
+def register_page(request):
+    return render(request, 'RegisterPage.html')
+
+def event_page(request):
+    return render(request, 'EventPage.html')
+
+def login_page(request):
+    return render(request, 'LoginPage.html')
+
+def club_page(request):
+    return render(request, 'ClubPage.html')
+
+def events_page(request):
+    return render(request, 'EventsPage.html')
+
+def about_page(request):
+    return render(request, 'AboutPage.html')    
+
+def chatbot_page(request):
+    return render(request, 'ChatBot.html')
