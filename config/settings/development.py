f9721ddc1908e035140144942e095558ed04d4f1
khouloud ben hlel <khouloudbenhelal24@gmail.com>
2024-11-09 18:23:07 +0100

    fix project

--- config/settings/development.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-development-secret-key' 
--- swaf/hackathon/apps.py
+++ swaf/hackathon/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SwafyConfig(AppConfig):
+class HackathonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'swafy'
+    name = 'hackathon'
